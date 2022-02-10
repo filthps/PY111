fbfef7c3d21dc26d86e057703b5f943ca7d83ff7
Литовченко Виктор Иванович <VIKLIT@avalon.ru>
2022-02-10 19:42:06 +0300

    Реализация структуры данных - стек.

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -6,7 +6,7 @@ from typing import Any
 
 class Stack:
     def __init__(self):
-        ...  # todo для стека можно использовать python list
+        self.stack = []
 
     def push(self, elem: Any) -> None:
         """
@@ -15,7 +15,7 @@ class Stack:
         :param elem: element to be pushed
         :return: Nothing
         """
-        print(elem)
+        self.stack.append(elem)
         return None
 
     def pop(self) -> Any:
@@ -24,7 +24,10 @@ class Stack:
 
         :return: popped element
         """
-        return None
+        if not self.stack:
+            return None
+        val = self.stack.pop()
+        return val
 
     def peek(self, ind: int = 0) -> Any:
         """
@@ -33,8 +36,12 @@ class Stack:
         :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
         :return: peeked element or None if no element in this place
         """
-        print(ind)
-        return None
+        if not self.stack:
+            return None
+        if ind >= len(self.stack):
+            return None
+        val = self.stack[-1 - ind]
+        return val
 
     def clear(self) -> None:
         """
@@ -42,4 +49,5 @@ class Stack:
 
         :return: None
         """
+        self.stack.clear()
         return None
