28f1b1be0f56e36239495c8b751c39d8eadb2f8e
Виктор Литовченко <filpsixi@mail.ru>
2022-02-17 20:36:14 +0300

    Числа фибоначчи. Итеративный варÐиант

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -16,5 +16,22 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    a = 0
+    b = 1
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+
+    for _ in range(2, n):
+        a, b = b, a + b
+    return b
+
+
+def fub_gen(n):
+    a, b = 0, 0
+    yield a
+    yield b
+    for i in range(n - 2):
+        a, b = b, a + b
+        yield b
