bc45f18d7d6e5815496ff7ba0339a8eec0f2312e
Виктор Литовченко <filpsixi@mail.ru>
2022-02-17 19:34:30 +0300

    Бинарный поиск. Сложность O(log2(n))

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,16 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    left-border = 0
+    rigth-border = len(arr) - 1
+
+    while left_border <= right_border:
+        middle_index = left_border + (right_border - left_border) // 2
+        middle_val = arr[middle_index]
+        if elem == middle_val:
+            return arr[elem]
+        elif elem < middle_index:
+            right_border = middle_index - 1
+        elif elem > middle_index:
+            left_border = middle_index + 1
+        return None
