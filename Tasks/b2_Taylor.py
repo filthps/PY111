cdf9e57ba056167bc45278c7e80096932832a6f2
Виктор Литовченко <filpsixi@mail.ru>
2022-02-17 19:53:03 +0300

    Ряд Маклрена (Ряд Тейлора)

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -1,7 +1,12 @@
+from math import factorial
+from itertools import  count
+
 """
 Taylor series
 """
 from typing import Union
+
+EPSILON = 0.00001  #
 
 
 def ex(x: Union[int, float]) -> float:
@@ -11,8 +16,15 @@ def ex(x: Union[int, float]) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
-    return 0
+    def get_item(x, n):
+        return x ** n / factorial(n)
+    sum_ = 0
+    for i in count(0, 1):
+        current_item = get_item(i)
+        sum_ += current_item
+        if current_item < EPSILON:
+            break
+    return sum_
 
 
 def sinx(x: Union[int, float]) -> float:
