71c40f725c0a451ead63ab0078644564344c80b9
Виктор Литовченко <filpsixi@mail.ru>
2022-03-03 19:08:39 +0300

    Ñe0

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,14 +1,29 @@
 from typing import Hashable, List
 import networkx as nx
+from collections import deque
 
 
 def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     """
+    Поиск в ширину (горизонтально)
     Do an breadth-first search and returns list of nodes in the visited order
 
     :param g: input graph
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    path_nodes = []
+    visited_nodes = {node: False for node in g.nodes}
+    wait_nodes = deque()  # "Горящие" ожидающие узлы
+    wait_nodes.append(start_node)
+    visited_nodes[start_node] = True
+    while wait_nodes:
+        curr_node = wait_nodes.popleft()
+        for neibhour in g[curr_node]:
+            if not visited_nodes[neibhour]:
+                wait_nodes.append(neibhour)
+                visited_nodes[neibhour] = True
+        path_nodes.append(curr_node)
+
+    return path_nodes
+
