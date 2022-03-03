f93829782b256442953cbb2d731da66150ff8aa3
Виктор Литовченко <filpsixi@mail.ru>
2022-03-03 20:01:42 +0300

    e1

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -10,5 +10,18 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    path_nodes = []
+    visited_nodes = {node: False for node in g.nodes}
+    wait_nodes = []  # "Горящие" ожидающие узлы
+    wait_nodes.append(start_node)
+    visited_nodes[start_node] = True
+    curr_node = wait_nodes.pop(-1)
+    if curr_node:
+        dfs(g, start_node)
+    for neibhour in g[curr_node]:
+        if not visited_nodes[neibhour]:
+            wait_nodes.append(neibhour)
+            visited_nodes[neibhour] = True
+    path_nodes.append(curr_node)
+
+    return path_nodes
