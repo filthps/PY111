f9b9e714d82e26da93fb168418a3fe8d91b90db6
Victor-PC <filpsixi@mail.ru>
2022-03-17 15:49:28 +0300

    g0

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -2,10 +2,18 @@ from typing import List
 
 
 def sort(container: List[int]) -> List[int]:
-    """
-    Sort input container with bubble sort
-
-    :param container: container of elements to be sorted
-    :return: container sorted in ascending order
-    """
+    for index in range(len(container)):
+        item = container[index]
+        try:
+            item_1 = container[index + 1]
+        except IndexError:
+            item_1 = None
+        if item_1 is not None:
+            print(item, item_1)
+            if item_1 < item:
+                container[index], container[index + 1] = item_1, item
     return container
+
+
+if __name__ == "__main__":
+    print([100, -2, 5, 4, 54, -34], sort([100, -2, 5, 4, 54, -34]))
