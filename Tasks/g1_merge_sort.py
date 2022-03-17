a7b86989f4545f9e6710f476a9ed347142be4f9a
Victor-PC <filpsixi@mail.ru>
2022-03-17 15:50:11 +0300

    g1

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -1,11 +1,22 @@
 from typing import List
+from itertools import chain
 
 
-def sort(container: List[int]) -> List[int]:
-    """
-    Sort input container with merge sort
+def sort(container: List[int]):
+    result = []
+    raw_left = []
+    raw_right = []
 
-    :param container: container of elements to be sorted
-    :return: container sorted in ascending order
-    """
-    return container
+    def slice_(left_sec, right_sec):
+
+
+    if len(container) > 1:  # Базовый случай
+        raw_left = slice_(container[:len(container) // 2], container[len(container) // 2:])
+        print(raw_left)
+    else:
+        return container
+    return
+
+
+if __name__ == "__main__":
+    print(sort([33, 6, 1, 343, 10]))
